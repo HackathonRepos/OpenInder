23e4c6cfe0981b90039cbfa311c383f71c6a8b97
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-03-19 17:46:35 -0700

    Added initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+import React from "react";
+
+import { Switch, Route, Redirect } from "react-router-dom";
 import "./styles/App.css";
+
+import Homepage from "./screens/Homepage";
+import SignIn from "./screens/SignIn";
+import SignUp from "./screens/SignUp";
+import Dashboard from "./screens/Dashboard";
+import Find from "./screens/Find";
+
+import { auth } from "./firebase";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" component={Homepage} exact />
+      <Route path="/signin" component={SignIn} exact />
+      <Route path="/signup" component={SignUp} exact />
+      <Route
+        path={"/authenticated/dashboard"}
+        render={() =>
+          auth.currentUser ? <Dashboard /> : <Redirect to="/signin" />
+        }
+        exact
+      />
+      <Route
+        path={"/authenticated/find"}
+        render={() => (auth.currentUser ? <Find /> : <Redirect to="/signin" />)}
+        exact
+      />
+    </Switch>
   );
 }
 
