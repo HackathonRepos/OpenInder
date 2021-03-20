32ee5e8225faf596b11a0a586879bd21a919f27c
AydanPirani <aydanpirani@gmail.com>
2021-03-19 23:08:05 -0700

    Added intial comments

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,14 @@ import { auth } from "./firebase";
 
 function App() {
   return (
+    // Routes (1 for each page we'll have in the website) 
     <Switch>
       <Route path="/" component={Homepage} exact />
       <Route path="/signin" component={SignIn} exact />
       <Route path="/signup" component={SignUp} exact />
-      <Route
+      <Route 
         path={"/authenticated/dashboard"}
+        // Check if the path is authenticated (i.e. user logged in), else redirect them to signin page
         render={() =>
           auth.currentUser ? <Dashboard /> : <Redirect to="/signin" />
         }
@@ -26,6 +28,7 @@ function App() {
       />
       <Route
         path={"/authenticated/find"}
+        // Check if the path is authenticated (i.e. user logged in), else redirect them to signin page
         render={() => (auth.currentUser ? <Find /> : <Redirect to="/signin" />)}
         exact
       />
