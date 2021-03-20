65cf4cecafc7e7e70a50f0d22b4b8ee50d7cfc6f
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-03-20 00:27:40 -0700

    Added rough layout of find component

--- src/screens/Find.jsx
+++ src/screens/Find.jsx
@@ -1,7 +1,86 @@
 import React from "react";
 
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
+import ClearIcon from "@material-ui/icons/Clear";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Box from "@material-ui/core/Box";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Link from "@material-ui/core/Link";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
 function Find() {
-  return <div>Find</div>;
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className={classes.screen}>
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            Dashboard
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.mainContent}>
+        <div className={classes.previous}></div>
+        <div className={classes.repoCard}></div>
+        <div className={classes.next}></div>
+      </div>
+    </div>
+  );
 }
+
+const useStyles = makeStyles((theme) => ({
+  screen: { backgroundColor: "#4C3B39", height: "100vh" },
+}));
 
 export default Find;
