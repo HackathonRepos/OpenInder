68ae8b038f18c3532ff2df4a5b456d1322869dc7
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-03-19 17:50:46 -0700

    Added boilerplate sign in/up forms

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
+
+// dont remove this auth as it initializes the firebase app as soon as the react app renders
 import { auth } from "./firebase";
 
 ReactDOM.render(
