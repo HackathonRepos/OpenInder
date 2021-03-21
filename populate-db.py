12084567f7648a14b29966a8e78723a8314aed2b
AydanPirani <aydanpirani@gmail.com>
2021-03-21 00:42:24 -0700

    Fixed Python code

--- populate-db.py
+++ populate-db.py
@@ -6,8 +6,9 @@ from firebase_admin import firestore
 # Get API token from env file
 def gen_api_token():
     f = open('.env', 'r')
-    api_token = f.readlines()[6].split("=")[1]
+    api_token = f.readlines()[6][:-1].split("=")[1]
     f.close()
+    print(api_token)
     return api_token
 
 g = Github(gen_api_token()) # Start GitHub instance
@@ -16,15 +17,14 @@ keywords = ["open source", "open-source"] # Query keywords (search for open-sour
 
 query = '+'.join(keywords) + '+in:readme+in:description'
 result = g.search_repositories(query, 'stars', 'desc') # Search for open-source databases and save in a list format
-max_repos = 10 # Measure of how many repos to be added to the DB
+max_repos = 99 # Measure of how many repos to be added to the DB
 print(f'Found {result.totalCount} repo(s)')
 
 repos = 0
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="secret.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./secret.json"
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred)
-
 db = firestore.client().collection("projects") # Connect to FireBase DB
 
 langs = set() # Set of all programming languages existing within the DB - analytic feature for filtering
@@ -42,7 +42,8 @@ for repo in result:
             "forks": int(repo.forks_count),
             "issues": int(repo.open_issues_count),
             "watchers": int(repo.watchers_count),
-            "subcscribers": int(repo.subscribers_count)
+            "subcscribers": int(repo.subscribers_count),
+            "stars": int(repo.stargazers_count),
         }
         langs.add(data["language"])
         if doc.get().to_dict() != data: # Checks if current repo is in DB -> if not the EXACT same, adds the newest version and proceeds
