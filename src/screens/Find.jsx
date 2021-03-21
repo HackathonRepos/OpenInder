28061df6efc53f84856ca27392c8fe2a6bc24224
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-03-20 19:26:25 -0700

    Added fade on click to card component on animation

--- src/screens/Find.jsx
+++ src/screens/Find.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -7,44 +7,53 @@ import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import BookmarkIcon from "@material-ui/icons/Bookmark";
 import ClearIcon from "@material-ui/icons/Clear";
+import IconButton from "@material-ui/core/IconButton";
+import Fade from "react-reveal/Fade";
+
+import firebase from "firebase";
 
 function Find() {
   const classes = useStyles();
+  const [increment, setIncrement] = useState(0);
   return (
     <div className={classes.screen}>
       <div className={classes.mainContent}>
         <div className={classes.swipe}>
-          <div>
+          <IconButton onClick={() => setIncrement(increment + 1)}>
             <ArrowBackIcon className={classes.previous} />
-          </div>
-          <div className={classes.repoCard}>
-            <div className={classes.repoCardHeader}>
-              <Typography variant="h3" style={{ display: "inline-flex" }}>
-                Repository Name
+          </IconButton>
+          <Fade spy={increment}>
+            <div className={classes.repoCard}>
+              <div className={classes.repoCardHeader}>
+                <Typography variant="h3" style={{ display: "inline-flex" }}>
+                  Repository Name
+                </Typography>
+                <BookmarkIcon />
+              </div>
+              <Typography>
+                This is a description of the repository stating what it does and
+                it is very cooll since this hackathon is so cool and I'm trying
+                to make this a decent long description to make sure I can format
+                this safely.
               </Typography>
-              <BookmarkIcon />
+              <Typography>Author: Author Name</Typography>
+              <Typography>Language: Javascript</Typography>
+              <Typography>Forks: Number of Forks</Typography>
+              <Typography>Check It out Here</Typography>
+              <div className={classes.bottomFacts}>
+                <Typography>Issues: Number of Issues</Typography>
+                <Typography>Watchers: Number of Watchers</Typography>
+                <Typography>Subscribers: Number of Subscribers</Typography>
+              </div>
             </div>
-            <Typography>
-              This is a description of the repository stating what it does and
-              it is very cooll since this hackathon is so cool and I'm trying to
-              make this a decent long description to make sure I can format this
-              safely.
-            </Typography>
-            <Typography>Author: Author Name</Typography>
-            <Typography>Language: Javascript</Typography>
-            <Typography>Forks: Number of Forks</Typography>
-            <Typography>Issues: Number of Issues</Typography>
-            <Typography>Watchers: Number of Watchers</Typography>
-            <Typography>SUbscribers: Number of Subscribers</Typography>
-            <Typography>Check It out Here</Typography>
-          </div>
-          <div>
+          </Fade>
+          <IconButton onClick={() => setIncrement(increment + 1)}>
             <ArrowForwardIcon className={classes.next} />
-          </div>
+          </IconButton>
         </div>
-        <div>
+        <IconButton onClick={() => setIncrement(increment + 1)}>
           <ClearIcon className={classes.block} />
-        </div>
+        </IconButton>
       </div>
     </div>
   );
@@ -79,13 +88,15 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)",
     display: "flex",
     flexDirection: "column",
+    marginBottom: "30px",
   },
   next: {
     color: "#F9F8F8",
     width: "75px",
     height: "75px",
   },
-  block: { color: "#a95962", width: "50px", height: "50px", marginTop: "30px" },
+  block: { color: "#a95962", width: "50px", height: "50px" },
+  bottomFacts: { display: "flex" },
 }));
 
 export default Find;
